d8dcb6f366780b8d63e547a2ca56be497fc9bce7
Adamya Gupta <adamyagupta005@gmail.com>
2026-02-04 22:53:10 +0530

    update database in backend

--- ToDo/backend/server.js
+++ ToDo/backend/server.js
@@ -1,31 +1,43 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 const path = require('path');
+const mongoose = require('mongoose');
 
 const app = express();
 app.use(bodyparser.urlencoded())
-app.use(express.static(path.join(__dirname, '..', 'frontend')));
+app.use(express.static(path.join(__dirname,'..','frontend')));
 
 app.set('view engine','ejs');
 
+mongoose.connect('mongodb://localhost:27017/todoDB');
 
-let todos = [];
-
-app.get('/',(req,res)=>{
-    res.render('index.ejs',{todos});
+const todoSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    }
 });
 
-app.post('/submit',(req,res)=>{
+const Todo = mongoose.model('todo',todoSchema);
+
+app.get('/',async (req,res)=>{
+    const todo = await Todo.find();
+    res.render('index.ejs',{todo});
+});
+
+app.post('/submit',async(req,res)=>{
     const todo = req.body.title;
-    todos.push(todo)
-    console.log(todos)
+    await Todo.create(
+        {title:todo}
+    )
+    console.log(Todo.find())
     res.redirect('/');
 });
-app.post('/submit/delete',(req,res)=>{
-    let index = req.body.index;
-    todos.splice(index,1);
+
+app.post('/submit/delete', async (req,res)=>{
+    await Todo.deleteOne({_id:req.body.id});
     res.redirect('/');
-})
+});
 
 app.listen(3000,()=>{
     console.log('http://localhost:3000');
